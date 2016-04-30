5aad66595c857901ee730d2608da4f6ea20232f0
Sander <sander@stamkracht.com>
2016-04-30 12:13:53 +0200

    added functionality to remove a world

--- src/app/routes/coordinator/worlds/settings/worlds.settings.controller.js
+++ src/app/routes/coordinator/worlds/settings/worlds.settings.controller.js
@@ -7,7 +7,12 @@
 
     /** @ngInject */
     function WorldsSettingsController(
+        $mdDialog,
+        $mdToast,
+        $state,
+        $stateParams,
         Global,
+        World,
         COORDINATOR_ACCESS_LEVEL
     ) {
 
@@ -16,12 +21,54 @@
             return;
         }
 
-        var vm = this;
+        var self = this;
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Methods
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        self.deleteWorld = deleteWorld;
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Services
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        World.getWorld($stateParams.worldUuid)
+            .then(function(response) {
+                self.world = response;
+            }, function() {
+                // Err
+            });
 
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Method Declarations
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        function deleteWorld(event) {
+            var dialog = $mdDialog.confirm()
+                        .title('Are you sure you want to delete this world?')
+                        .textContent('Please consider your answer, this action can not be undone.')
+                        .clickOutsideToClose(true)
+                        .ariaLabel('Delete world')
+                        .targetEvent(event)
+                        .ok('Yes, I accept the consequences')
+                        .cancel('No, take me back!');
+
+            $mdDialog.show(dialog).then(function() {
+                World.deleteWorld(self.world.uuid)
+                    .then(function(response) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                            .textContent('World ' + self.world.name + ' has been deleted')
+                            .position('bottom right')
+                            .hideDelay(3000)
+                        );
+                        $state.go('base.worlds.overview');
+                    }, function() {
+                        // Err
+                    });
+            }, function() {
+                // No
+            });
+        }
 
     }
 
