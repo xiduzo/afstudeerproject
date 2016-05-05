aeffe8dcc9aa0e23288c738dc0a843d6bf5bf466
Sander <sander@stamkracht.com>
2016-05-05 12:13:40 +0200

    php services to create, patch and delete guild

---
+++ src/php_services/delete/guild.php
@@ -0,0 +1,27 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    require_once '../config.php';
+
+    $uuid = $_GET['uuid'];
+
+    if(empty($uuid)) {
+        echo json_encode(false);
+        return;
+    }
+
+    // Insert the house to the database
+    $database->delete("Guild", [
+        "uuid" => $uuid
+    ]);
+
+    // Also delete all the connection this guild had in the DB
+    $database->delete("UserInGuild", [
+        "guildUuid" => $uuid
+    ]);
+
+    echo json_encode(true);
+
+?>
---
+++ src/php_services/get/guild.php
@@ -0,0 +1,20 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    require_once '../config.php';
+
+    $uuid = $_GET['uuid'];
+
+    if(empty($uuid)) {
+        echo json_encode(false);
+        return;
+    }
+
+    $guild = $database->get("Guild", "*", [
+        "uuid" => $uuid
+    ]);
+
+    echo json_encode($guild);
+
+?>
