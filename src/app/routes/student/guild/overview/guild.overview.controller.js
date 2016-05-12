7925c5f90c637ba82edbbaa656815d73279f30aa
Sander <sander@stamkracht.com>
2016-05-12 19:43:24 +0200

    playing with highcharts on the guild overview page

--- src/app/routes/student/guild/overview/guild.overview.controller.js
+++ src/app/routes/student/guild/overview/guild.overview.controller.js
@@ -22,6 +22,7 @@
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Methods
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        self.createExperienceChart = createExperienceChart;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Variables
@@ -42,6 +43,9 @@
                             _.each(response, function(member) {
                                 guild.members.push(member);
                             });
+
+                            // Create the experience table shit
+                            self.createExperienceChart(guild);
                         }, function() {
                             // Err
                         });
@@ -53,6 +57,82 @@
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Method Declarations
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        function createExperienceChart(guild) {
+            $('#'+guild.uuid).highcharts({
+
+                chart: {
+                    backgroundColor: 'rgba(0,0,0,0)'
+                },
+
+                title: {
+                    text: 'Experience for ' +guild.name,
+                    x: -20 //center
+                },
+
+                xAxis: {
+                    title: {
+                        text: 'Weken'
+                    },
+                    categories: ['Week 0', 'Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Herkansing']
+                },
+
+                yAxis: {
+                    title: {
+                        text: 'Experience'
+                    },
+                    plotLines: [{
+                        value: 55000,
+                        width: 1,
+                        color: '#808080',
+                        label: {
+                            text: 'Minimal experience needed to pass'
+                        }
+                    }],
+                    // TODO
+                    // Set max exp for a grade 10
+                    min: 0,
+                    max: 100000
+                },
+
+                exporting: {
+                    // Only show the exporting button when you have a higher access level than the student
+                    enabled: false,
+                },
+
+                legend: {
+                    enabled: false
+                },
+
+                tooltip: {
+                    shared: false,
+                    pointFormat: 'Experience: <strong>{point.y:,.0f}</strong>'
+                },
+
+                series: [
+                    {
+                        name: 'dase_data',
+                        color: 'rgba(0,0,0,0)',
+                        data: [
+                            -1, -1, -1, -1, -1, -1, -1, -1
+                        ]
+                    },
+                    {
+                        name: guild.name,
+                        data: [
+                            0,
+                            Math.floor(Math.random() * 10000 ),
+                            Math.floor(Math.random() * 5000 + 5000),
+                            Math.floor(Math.random() * 10000 + 10000),
+                            Math.floor(Math.random() * 15000 + 15000),
+                            Math.floor(Math.random() * 20000 + 20000),
+                            Math.floor(Math.random() * 40000 + 40000),
+                            Math.floor(Math.random() * 40000 + 60000),
+                        ],
+                        color: '#FFCC00'
+                    }
+                ]
+            });
+        }
 
     }
 
