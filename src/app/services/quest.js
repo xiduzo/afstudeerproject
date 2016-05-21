03109a0511ddb9295a9866388843a31db2ed648a
Sander <sander@stamkracht.com>
2016-05-21 21:21:02 +0200

    added some frontend services

--- src/app/services/quest.js
+++ src/app/services/quest.js
@@ -19,6 +19,9 @@
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         service.addQuest = addQuest;
         service.getQuests = getQuests;
+        service.deleteQuest = deleteQuest;
+        service.getQuest = getQuest;
+        service.patchQuest = patchQuest;
 
         return service;
 
@@ -67,6 +70,69 @@
                 });
             });
         }
+
+        function deleteQuest(quest, world) {
+            return $q(function(resolve, reject) {
+                $http({
+                    url: API_URL + 'delete/quest.php',
+                    method: "GET",
+                    params: {
+                        questUuid: quest,
+                        worldUuid: world
+                    }
+                })
+                .then(function(response) {
+                    resolve(response.data);
+                }, function(error) {
+                    reject(error);
+                });
+            });
+        }
+
+        function getQuest(quest) {
+            return $q(function(resolve, reject) {
+                $http({
+                    url: API_URL + 'get/quest.php',
+                    method: "GET",
+                    params: {
+                        questUuid: quest
+                    }
+                })
+                .then(function(response) {
+                    resolve(response.data);
+                }, function(error) {
+                    reject(error);
+                });
+            });
+        }
+
+        function patchQuest(quest, world) {
+            return $q(function(resolve, reject) {
+                $http({
+                    url: API_URL + 'patch/quest.php',
+                    method: "GET",
+                    params: {
+                        questUuid: quest.uuid,
+                        name: quest.name,
+                        experience: quest.experience,
+                        description: quest.description,
+                        id: quest.skills.interaction_design,
+                        vid: quest.skills.visual_interface_design,
+                        fd: quest.skills.frontend_development,
+                        cm: quest.skills.content_management,
+                        pm: quest.skills.project_management,
+                        worldUuid: world
+                    }
+                })
+                .then(function(response) {
+                    resolve(response.data);
+                }, function(error) {
+                    reject(error);
+                });
+            });
+        }
+
+
 
     }
 
