05474b7f14ba21a8120fa8507c8eb93114c51c01
Sander <sander@stamkracht.com>
2016-09-01 20:57:49 +0200

    almost done creating the objectivs graph work, need to loop trough all the dates not just tje objective dates

--- src/app/routes/student/guild/overview/guild.overview.controller.js
+++ src/app/routes/student/guild/overview/guild.overview.controller.js
@@ -29,6 +29,7 @@
         self.createExperienceChart = createExperienceChart;
         self.addObjective = addObjective;
         self.removeObjective = removeObjective;
+        self.buildGraphData = buildGraphData;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Variables
@@ -36,31 +37,11 @@
         self.user = Global.getUser();
         self.guilds = [];
 
-        var exp_data = [];
         var quest_points = [];
         var categories = [];
         var weeknumber = 0;
-        var objectivesGraphLine = [];
-
-        exp_data = [
-            0, 20, 60, 60, 60, 60, 60,
-            140, 155, 140, 140, 140, 140, 140,
-            200, 230, 245, 245, 245, 245, 245,
-            245, 270, 280, 280, 280, 280, 280,
-            400, 430, 430, 430, 430, 430, 430,
-            570, 580, 590, 590, 590, 590, 590,
-            750, 780, 760, 760, 760, 760, 760,
-            760
-        ];
-
-        for(var c = 0; c <= 49; c++) {
-            if(c === 0 || c % 7 === 0) {
-                categories.push('Week ' + weeknumber);
-                weeknumber++;
-            } else {
-                categories.push(c);
-            }
-        }
+        var objectives_graph_line = [];
+        var horizontal_axis = [];
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Services
@@ -100,64 +81,9 @@
                         // Err get quests
                     });
 
-                    // Building the scrum chart
-                    var objectivePoints = [];
-                    _.each(guild.objectives, function(objective) {
-                        var tempObject = {
-                            date: moment(objective.created_at).format(),
-                            points: objective.points,
-                            completed_at: objective.completed_at
-                        };
-                        objectivePoints.push(tempObject);
-                    });
-
-                    objectivePoints = _.groupBy(objectivePoints, function(op) {
-                        return moment(op.date).format('DD/MM/YY');
-                    });
-
-
-                    // TODO
-                    // Make the graph work
-                    // Step 1: just add all the values with corrosponding date
-                    // Step 2: adding the last days points to this days points
-                    // Step 3: if you are on / over de completion date of an
-                    // previous objetive -> substract the points of this
-                    // Step 4: Add the new data on the chart (use only the
-                    // objectivesGraphLine var)
-                    // Step 5: drink beer, you deserve one ;)
-
-
-                    // Adding all the values together
-                    _.each(objectivePoints, function(objectivePoint) {
-                        var reduced = { date: null, points: 0 };
-                        _.each(objectivePoint, function(dateAndPoints) {
-                            reduced = {
-                                date: moment(dateAndPoints.date).format('DD/MM/YY'),
-                                points: reduced.points + dateAndPoints.points
-                            };
-                        });
-                        objectivesGraphLine.push(reduced);
-                    });
-
-                    console.log(objectivesGraphLine);
-
-                    quest_points = [
-                        1000, 1000, 970, 940, 930, 930, 930,
-                        930, 930, 840, 840, 840, 760, 730,
-                        700, 700, 700, 700, 600, 600, 600,
-                        600, 600, 580, 520, 420, 420, 420,
-                        420, 420, 420, 400, 400, 300, 200,
-                        200, 200, 200, 200, 150, 150, 150,
-                        150, 150, 150, 150, 150, 150, 100,
-                        100
-                    ];
-
-                    // Finaly display all the data to the user
                     self.guilds.push(guild);
+                    self.buildGraphData(response, guild);
 
-                    setTimeout(function () {
-                        self.createExperienceChart(guild.id);
-                    }, 100);
                 });
             });
         }, function() {
@@ -167,6 +93,158 @@
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Method Declarations
         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        function buildGraphData(world, guild) {
+            var objectives = guild.objectives;
+            var previous_day_data = [];
+            var objectives_graph_items = [];
+            var starting_date = moment(world.start).format();
+            var course_duration = world.course_duration;
+            var weeknumber = 1;
+            var previous_points = null;
+            guild.objective_points = 0;
+            guild.completed_objective_points = 0;
+
+
+            // Building the horizontal axis of the graph (date)
+            for(var i = 0; i <= course_duration; i++) {
+                if(i === 0 || i % 7 === 0) {
+                    horizontal_axis.push('<b>Week ' + weeknumber + '</b>');
+                    weeknumber++;
+                } else {
+                    horizontal_axis.push(moment(starting_date)
+                        .add(i, 'day')
+                        .format('DD/MM')
+                    );
+                }
+                var tempObj = {
+                    date: moment(starting_date)
+                        .add(i, 'day')
+                        .format('DD/MM'),
+                    points: null
+                };
+            }
+
+            var test_data = [
+                {
+                    "completed": true,
+                    "completed_at": "2016-08-21T14:36:38+02:00",
+                    created_at: "2016-08-10T14:36:38+02:00",
+                    "points": 200,
+                    "name": "name",
+                    "description": "description"
+                },
+                {
+                    completed: true,
+                    completed_at: "2016-08-23T14:36:38+02:00",
+                    created_at: "2016-08-11T14:36:38+02:00",
+                    points: 200,
+                    "name": "name",
+                    "description": "description"
+                },
+                {
+                    completed: true,
+                    completed_at: "2016-08-27T14:36:38+02:00",
+                    created_at: "2016-08-12T14:36:38+02:00",
+                    points: 200,
+                    "name": "name",
+                    "description": "description"
+                },
+                {
+                    completed: false,
+                    completed_at: null,
+                    created_at: "2016-08-13T14:36:38+02:00",
+                    points: 200,
+                    "name": "name",
+                    "description": "description"
+                },
+                {
+                    completed: false,
+                    completed_at: null,
+                    created_at: "2016-08-14T14:36:38+02:00",
+                    points: 200,
+                    "name": "name",
+                    "description": "description"
+                },
+                {
+                    completed: false,
+                    completed_at: null,
+                    created_at: "2016-08-14T14:36:38+02:00",
+                    points: 200,
+                    "name": "name",
+                    "description": "description"
+                },
+            ];
+
+            _.each(test_data, function(data) {
+                objectives.push(data);
+            });
+
+            // Group the points by date
+            objectives = _.groupBy(objectives, function(objective) {
+                return moment(objective.created_at).format('DD/MM/YY');
+            });
+
+            // Union all the objectives when time progresses
+            _.each(objectives, function(objective, index) {
+                if(previous_day_data.length) {
+                    objective = _.union(previous_day_data, objective);
+                }
+                previous_day_data = objective;
+
+                // Overwrite the objectives
+                objectives[index] = objective;
+            });
+
+            // Adding all the values together
+            _.each(objectives, function(group) {
+                var reduced = _.reduce(group, function(memo, num) {
+                    memo.date = _.last(group).created_at;
+
+                    // Only add the points if the objective isnt completed yet OR
+                    // if the completion date is after the group date
+                    if(num.completed &&
+                        moment(num.completed_at)
+                        .isSameOrBefore(memo.date)
+                    ) {
+                        memo.points += 0;
+                    } else {
+                        memo.points += num.points;
+                    }
+                    return memo;
+                }, { date: null, points: 0 });
+
+                reduced.date = moment(reduced.date).format('DD/MM');
+                // Add the item to the grapsh items
+                console.log(reduced);
+                objectives_graph_items.push(reduced);
+            });
+
+            for(i = 0; i <= course_duration; i++) {
+                var date = moment(starting_date).add(i, 'day');
+                var match = _.findWhere(
+                    objectives_graph_items,
+                    {date: date.format('DD/MM')}
+                );
+                if(match) {
+                    previous_points = match.points;
+                    objectives_graph_line.push(match.points);
+                } else {
+                    if(moment().isBefore(date)) {
+                        objectives_graph_line.push(null);
+                    } else {
+                        objectives_graph_line.push(previous_points);
+                    }
+                }
+            }
+
+            setTimeout(function () {
+                self.createExperienceChart(guild);
+            }, 100);
+
+        }
+
+        // TODO
+        // Update graph on success
         function addObjective(guild) {
             $mdDialog.show({
                 controller: 'addObjectiveController',
@@ -210,84 +288,50 @@
             });
         }
 
+        // TODO
+        // Update graph on success
         function removeObjective(guild, objective) {
             Guild.removeObjective(objective.id)
             .then(function(response) {
                 guild.objectives.splice(guild.objectives.indexOf(objective), 1);
                 $mdToast.show(
                     $mdToast.simple()
-                    .textContent('Objective ' + response.name + ' removed')
+                    .textContent('Objective ' + objective.name + ' removed')
                     .position('bottom right')
                     .hideDelay(3000)
                 );
-                console.log(response);
             }, function(error) {
                 // Err remove objective
             });
         }
 
         function createExperienceChart(guild) {
-            $('#'+guild).highcharts({
-
-                chart: {
-                    backgroundColor: 'rgba(0,0,0,0)'
-                },
-
-                title: {
-                    text: 'Progress'
-                },
-
+            $('#'+guild.id).highcharts({
+                chart: { backgroundColor: 'rgba(0,0,0,0)' },
+                title: { text: 'Progress of ' + guild.name },
                 xAxis: {
-                    title: {
-                        text: 'Dagen'
-                    },
-                    categories: categories
+                    title: { text: 'Dagen' },
+                    categories: horizontal_axis
                 },
-
-                yAxis: [
-                    {
-                        title: {
-                            text: 'Experience'
-                        },
-                        min: 0
-                    },
-                    {
-                        title: {
-                            text: 'Remaning quest points'
-                        },
-                        min: 0,
-                        opposite: true
-                    },
-                ],
-
-                exporting: {
-                    // Only show the exporting button when you have a higher access level than the student
-                    enabled: false,
+                yAxis: {
+                    title: { text: 'Remaning objective points' },
+                    min: 0
                 },
-
-                legend: {
-                    enabled: false
-                },
-
+                // Only show the exporting button when you have
+                // a higher access level than the student
+                exporting: { enabled: false, },
+                legend: { enabled: false },
                 tooltip: {
                     shared: false,
                     pointFormat: '{series.name}: <strong>{point.y:,.0f}</strong>'
                 },
-
                 series: [
                     {
-                        name: 'Experience',
-                        data: exp_data,
-                        color: '#FFCC00'
-                    },
-                    {
                         name: 'Remaining quest points',
-                        data: quest_points,
-                        color: '#cd2327',
-                        yAxis: 1
+                        data: objectives_graph_line,
+                        color: '#cd2327'
                     }
                 ],
-
                 credits: {
                     text: moment().format("DD/MM/YY HH:MM"),
                     href: ''
