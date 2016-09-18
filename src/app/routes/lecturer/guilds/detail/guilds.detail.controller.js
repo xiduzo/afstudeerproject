42e1711988b5ca9bd805cdc0fc748424fbf5f8d8
Sander <sander@stamkracht.com>
2016-09-18 15:38:45 +0200

    refactored the graph data

--- src/app/routes/lecturer/guilds/detail/guilds.detail.controller.js
+++ src/app/routes/lecturer/guilds/detail/guilds.detail.controller.js
@@ -11,6 +11,7 @@
         $state,
         Guild,
         Global,
+        Notifications,
         World,
         LECTURER_ACCESS_LEVEL
     ) {
@@ -59,7 +60,7 @@
         Guild.getGuild($stateParams.guildUuid)
             .then(function(response) {
                 if(!response) {
-                    GLobal.simpleToast('Group ' + $stateParams.guildUuid + ' does not exist');
+                    Notifications.simpleToast('Group ' + $stateParams.guildUuid + ' does not exist');
                     $state.go('base.guilds.overview');
                 }
 
@@ -74,7 +75,7 @@
                 });
 
             }, function() {
-                // Err
+                // Err get guild
             });
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -84,9 +85,9 @@
             Guild.patchQuestStatus(quest)
             .then(function(response) {
                 if(response.completed) {
-                    Global.simpleToast('Asignment marked as completed');
+                    Notifications.simpleToast('Asignment marked as completed');
                 } else {
-                    Global.simpleToast('Asignment marked as uncompleted');
+                    Notifications.simpleToast('Asignment marked as uncompleted');
                 }
             }, function(error) {
                 // Err patch quest completion status
@@ -94,144 +95,99 @@
         }
 
         function prepareGraphData(guild) {
-            var course_duration = guild.world.course_duration;
-            var starting_date = guild.world.start;
+            var graph_data = {
+                weeknumber: 0,
+                column_dates: [],
+                horizontal_axis: [],
+                series: [],
+                points: [],
+            };
 
-            var pie_graph_data = [];
-            var bar_graph_data = [];
-
-            var weeknumber = 0;
-            var total_points = 0;
-
-            var column_dates = [];
-            var user_groups = [];
-
-            bar_graph_data.horizontal_axis = [];
-            bar_graph_data.series = [];
             // Only need the completed objectives
             guild.objectives = _.filter(guild.objectives, {completed: true});
 
-            // Prepare the points per person
-            _.each(guild.members, function(member) {
-                var tempObj = { id: member.id, points: 0 };
-                if(member.surname_prefix) {
-                    tempObj.name = member.first_name + ' ' + member.surname_prefix + ' ' + member.surname;
-                } else {
-                    tempObj.name = member.first_name + ' ' + member.surname;
-                }
-                user_groups.push(tempObj);
+            // Prepare the graph data points
+            _.each(guild.members, function(member, index) {
+                var tempObj = {
+                    id: member.id,
+                    color: self.colors[index],
+                    name: member.first_name + ' ' + member.surname_prefix + ' ' + member.surname,
+                    y: 0, // Pie chart
+                    data: [], // Column graph
+                };
+
+                // Need to have two seperate arrays b/c
+                // We are gonna build two different charts
+                graph_data.points.push(tempObj);
+                graph_data.series.push(tempObj);
             });
 
             // Building the horizontal axis of the graph (date)
-            for(var i = 0; i <= course_duration; i++) {
+            for(var i = 0; i <= guild.world.course_duration; i++) {
+                var date = moment(guild.world.start).add(i, 'day');
+                graph_data.column_dates.push({date: date.format()});
+
                 if(i === 0 || i % 7 === 0) {
-                    bar_graph_data.horizontal_axis.push('<b>Week ' + weeknumber + '</b>');
-                    weeknumber++;
+                    graph_data.horizontal_axis.push('<b>Week ' + graph_data.weeknumber + '</b>');
+                    graph_data.weeknumber++;
                 } else {
-                    bar_graph_data.horizontal_axis.push(moment(starting_date)
-                        .add(i, 'days').format('DD/MM')
-                    );
+                    graph_data.horizontal_axis.push(date.format('DD/MM'));
                 }
-                var tempObj = {
-                    date: moment(starting_date)
-                        .add(i, 'day').format(),
-                };
-                column_dates.push(tempObj);
             }
 
-            // Fill in the bar graph series
-            _.each(user_groups, function(person, index) {
-                var tempObj = {
-                    user_id: person.id,
-                    name: person.name,
-                    color: self.colors[index],
-                    data: [],
-                };
-                bar_graph_data.series.push(tempObj);
-            });
-
-            _.map(column_dates, function(date, index) {
+            // Building the column grah
+            _.each(graph_data.column_dates, function(date, index) {
                 // Building the base
-                _.each(bar_graph_data.series, function(serie) {
+                _.each(graph_data.series, function(serie) {
                     serie.data.push(null);
                 });
 
                 _.each(guild.objectives, function(objective) {
                     if(moment(date.date).isSame(objective.completed_at, 'day')) {
-                        _.each(objective.assignments, function(assigned) {
-                            var serie = _.find(bar_graph_data.series, {
-                                user_id: assigned.user.id
+                        if(objective.assignments.length < 1) {
+                            _.each(graph_data.series, function(serie) {
+                                serie.data[index] += objective.points / guild.members.length;
                             });
-                            serie.data[index] += objective.points / objective.assignments.length;
-                        });
+                        } else {
+                            _.each(objective.assignments, function(assigned) {
+                                var serie = _.find(graph_data.series, {
+                                    id: assigned.user.id
+                                });
+                                serie.data[index] += objective.points / objective.assignments.length;
+                            });
+                        }
                     }
                 });
-
             });
 
+            // Building the pie chart
             _.each(guild.objectives, function(objective) {
-                total_points += objective.points;
                 if(objective.assignments.length < 1) {
-                    _.map(user_groups, function(user, key) {
-                        return user.points += objective.points;
+                    _.each(graph_data.points, function(user) {
+                        user.points += objective.points;
                     });
                 } else {
                     _.each(objective.assignments, function(assigned) {
                         // Adding the points to the user
-                        _.map(user_groups, function(person) {
+                        _.each(graph_data.points, function(person) {
                             if(assigned.user.id == person.id) {
-                                person.points += objective.points / objective.assignments.length;
+                                person.y += objective.points / objective.assignments.length;
                             }
                         });
                     });
                 }
             });
 
-            _.map(user_groups, function(person, index) {
-                var tempObj = {
-                    name: person.name,
-                    y: Math.random() * 25 + 5,
-                    points: person.points,
-                    color: self.colors[index],
-                };
-                pie_graph_data.push(tempObj);
-            });
-
-            self.buildGraphs(bar_graph_data, pie_graph_data);
+            self.buildGraphs(graph_data);
         }
 
-        function buildGraphs(bar_data, pie_data) {
+        function buildGraphs(graph_data) {
             $('#completed__tasks').highcharts({
-                chart: {
-                    type: 'column'
-                },
-                exporting: {
-                    // Only show the exporting button when you have a higher access level than the student
-                    enabled: Global.getAccess() > 1 ? true : false
-                },
-                title: {
-                    text: 'Completed tasks ' + self.guild.name
-                },
-                xAxis: {
-                    categories: bar_data.horizontal_axis
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Total points earned'
-                    },
-                },
-                legend: {
-                    align: 'right',
-                    x: -30,
-                    verticalAlign: 'top',
-                    y: 25,
-                    floating: true,
-                    backgroundColor: 'white',
-                    borderColor: '#CCC',
-                    borderWidth: 1,
-                    shadow: false
-                },
+                chart: { type: 'column' },
+                exporting: { enabled: Global.getAccess() > 1 ? true : false },
+                title: { text: 'Completed tasks ' + self.guild.name },
+                xAxis: { categories: graph_data.horizontal_axis },
+                yAxis: { title: { text: 'Total points earned' }, },
                 tooltip: {
                     headerFormat: '<b>{point.x}</b><br/>',
                     pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
@@ -243,27 +199,15 @@
                         stacking: 'normal',
                     }
                 },
-                series: bar_data.series,
-                credits: {
-                    text: moment().format("DD/MM/YY HH:mm"),
-                    href: ''
-                }
+                series: graph_data.series,
+                credits: { text: moment().format("DD/MM/YY HH:mm"), href: '' }
             });
 
             $('#completed__tasks__explained').highcharts({
-                chart: {
-                    type: 'pie'
-                },
-                exporting: {
-                    // Only show the exporting button when you have a higher access level than the student
-                    enabled: Global.getAccess() > 1 ? true : false
-                },
-                title: {
-                    text: 'Completion per user'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.points}</b>'
-                },
+                chart: { type: 'pie' },
+                exporting: { enabled: Global.getAccess() > 1 ? true : false },
+                title: { text: 'Completion per user' },
+                tooltip: { pointFormat: '{series.name}: <b>{point.y}</b>' },
                 plotOptions: {
                     pie: {
                         // Maybe for future use
@@ -274,16 +218,8 @@
                         }
                     }
                 },
-                series: [
-                    {
-                        name: 'Points',
-                        data: pie_data
-                    }
-                ],
-                credits: {
-                    text: moment().format("DD/MM/YY HH:mm"),
-                    href: ''
-                }
+                series: [ { name: 'Points', data: graph_data.points } ],
+                credits: { text: moment().format("DD/MM/YY HH:mm"), href: '' }
             });
         }
 
