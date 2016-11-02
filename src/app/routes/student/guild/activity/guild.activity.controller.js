ca9f85fe152e252982065759ee43879aedecb488
Sander <sander@stamkracht.com>
2016-11-02 13:48:35 +0100

    some cleanups

--- src/app/routes/student/guild/activity/guild.activity.controller.js
+++ src/app/routes/student/guild/activity/guild.activity.controller.js
@@ -136,6 +136,8 @@
 
                         });
                     });
+                } else {
+                    self.guilds.push(guild);
                 }
             });
         }, function() {
