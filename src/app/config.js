20bd93b1afe9a24b0e96d37f98fad8e9d6ee5f47
Sander <mail@sanderboer.nl>
2017-03-14 12:15:55 +0100

    added dutch moment locate

--- src/app/config.js
+++ src/app/config.js
@@ -287,6 +287,74 @@
             name: 'CMD Athena'
         });
 
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Momnent
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        moment.defineLocale('nl', {
+            months : 'januari_februari_maart_april_mei_juni_juli_augustus_september_oktober_november_december'.split('_'),
+            monthsShort : function (m, format) {
+                if (/-MMM-/.test(format)) {
+                    return 'jan_feb_mrt_apr_mei_jun_jul_aug_sep_okt_nov_dec'.split('_')[m.month()];
+                } else {
+                    return 'jan_feb_mrt_apr_mei_jun_jul_aug_sep_okt_nov_dec'.split('_')[m.month()];
+                }
+            },
+
+            monthsRegex: /^(januari|februari|maart|april|mei|april|ju[nl]i|augustus|september|oktober|november|december|jan\.?|feb\.?|mrt\.?|apr\.?|ju[nl]\.?|aug\.?|sep\.?|okt\.?|nov\.?|dec\.?)/i,
+            monthsShortRegex: /^(januari|februari|maart|april|mei|april|ju[nl]i|augustus|september|oktober|november|december|jan\.?|feb\.?|mrt\.?|apr\.?|ju[nl]\.?|aug\.?|sep\.?|okt\.?|nov\.?|dec\.?)/i,
+            monthsStrictRegex: /^(januari|februari|maart|mei|ju[nl]i|april|augustus|september|oktober|november|december)/i,
+            monthsShortStrictRegex: /^(jan\.?|feb\.?|mrt\.?|apr\.?|mei|ju[nl]\.?|aug\.?|sep\.?|okt\.?|nov\.?|dec\.?)/i,
+
+            monthsParse : [/^jan/i, /^feb/i, /^maart|mrt.?$/i, /^apr/i, /^mei$/i, /^jun[i.]?$/i, /^jul[i.]?$/i, /^aug/i, /^sep/i, /^okt/i, /^nov/i, /^dec/i],
+            longMonthsParse : [/^jan/i, /^feb/i, /^maart|mrt.?$/i, /^apr/i, /^mei$/i, /^jun[i.]?$/i, /^jul[i.]?$/i, /^aug/i, /^sep/i, /^okt/i, /^nov/i, /^dec/i],
+            shortMonthsParse : [/^jan/i, /^feb/i, /^maart|mrt.?$/i, /^apr/i, /^mei$/i, /^jun[i.]?$/i, /^jul[i.]?$/i, /^aug/i, /^sep/i, /^okt/i, /^nov/i, /^dec/i],
+
+            weekdays : 'zondag_maandag_dinsdag_woensdag_donderdag_vrijdag_zaterdag'.split('_'),
+            weekdaysShort : 'zo._ma._di._wo._do._vr._za.'.split('_'),
+            weekdaysMin : 'Zo_Ma_Di_Wo_Do_Vr_Za'.split('_'),
+            weekdaysParseExact : true,
+            longDateFormat : {
+                LT : 'HH:mm',
+                LTS : 'HH:mm:ss',
+                L : 'DD-MM-YYYY',
+                LL : 'D MMMM YYYY',
+                LLL : 'D MMMM YYYY HH:mm',
+                LLLL : 'dddd D MMMM YYYY HH:mm'
+            },
+            calendar : {
+                sameDay: '[vandaag om] LT',
+                nextDay: '[morgen om] LT',
+                nextWeek: 'dddd [om] LT',
+                lastDay: '[gisteren om] LT',
+                lastWeek: '[afgelopen] dddd [om] LT',
+                sameElse: 'L'
+            },
+            relativeTime : {
+                future : 'over %s',
+                past : '%s geleden',
+                s : 'een paar seconden',
+                m : 'één minuut',
+                mm : '%d minuten',
+                h : 'één uur',
+                hh : '%d uur',
+                d : 'één dag',
+                dd : '%d dagen',
+                M : 'één maand',
+                MM : '%d maanden',
+                y : 'één jaar',
+                yy : '%d jaar'
+            },
+            ordinalParse: /\d{1,2}(ste|de)/,
+            ordinal : function (number) {
+                return number + ((number === 1 || number === 8 || number >= 20) ? 'ste' : 'de');
+            },
+            week : {
+                dow : 1, // Monday is the first day of the week.
+                doy : 4  // The week that contains Jan 4th is the first week of the year.
+            }
+        });
+        moment.locale('nl');
+
 
     }
 }());
