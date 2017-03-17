951068c8b54582b46b48a30ea558883c3cb96548
Sander <mail@sanderboer.nl>
2017-03-17 15:29:34 +0100

    removed hashbang from routing

--- src/app/routes/base/base.setup.js
+++ src/app/routes/base/base.setup.js
@@ -8,7 +8,8 @@
 
     /** @ngInject */
     function config(
-        $stateProvider
+        $stateProvider,
+        $locationProvider
     ) {
 
         // add the needed routes to the state provider
@@ -22,6 +23,11 @@
                     controllerAs: 'baseCtrl'
                 }
             }
+        });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            // requireBase: false
         });
 
     }
