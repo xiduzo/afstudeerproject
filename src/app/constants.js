098ccdb15acae2d3d44e407771486e4601cee11c
sander boer <mail@sanderboer.nl>
2019-08-21 21:34:01 +0200

    Added dynamic trello user

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,72 +1,75 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    angular.module('cmd.constants', [])
+  angular
+    .module("cmd.constants", [])
 
-        .constant('DEBUG_ENABLED', true)
+    .constant("DEBUG_ENABLED", true)
 
-        .constant('LDAP_LOGIN_API', 'https://oege.ie.hva.nl/~vddxx/ldapper/index.php')
-        // .constant('REST_API_URL', 'http://127.0.0.1:8000/api/')
-        .constant('REST_API_URL', 'https://back.beyond.jstur.org/api/')
+    .constant(
+      "LDAP_LOGIN_API",
+      "https://oege.ie.hva.nl/~vddxx/ldapper/index.php"
+    )
+    .constant("REST_API_URL", "http://127.0.0.1:8000/api/")
+    // .constant('REST_API_URL', 'https://back.beyond.jstur.org/api/')
 
-        .constant('CREDENTIAL_USER', 'bigd')
-        .constant('CREDENTIAL_PASS', 'aapnootbier')
+    .constant("CREDENTIAL_USER", "bigd")
+    .constant("CREDENTIAL_PASS", "aapnootbier")
 
-        .constant('TRELLO_KEY', '85ea9af753540fb15c161d5eedd67a49')
-        .constant('TRELLO_SECRET', '92b46d2d43997521d21894e6f88b843ddae5d903163d813356cfa7b376a85e2b')
-        .constant('TRELLO_USER_ID', '5810c2cf05262a2228ff17ae')
+    .constant("TRELLO_KEY", "85ea9af753540fb15c161d5eedd67a49")
+    .constant(
+      "TRELLO_SECRET",
+      "92b46d2d43997521d21894e6f88b843ddae5d903163d813356cfa7b376a85e2b"
+    )
 
-        .constant('STUDENT_ACCESS_LEVEL', 1)
-        .constant('LECTURER_ACCESS_LEVEL', 2)
-        .constant('COORDINATOR_ACCESS_LEVEL', 3)
+    .constant("STUDENT_ACCESS_LEVEL", 1)
+    .constant("LECTURER_ACCESS_LEVEL", 2)
+    .constant("COORDINATOR_ACCESS_LEVEL", 3)
 
-        .constant('MAX_STAR_RATING', 4)
+    .constant("MAX_STAR_RATING", 4)
 
-        .constant('HTTP_STATUS', {
-            SUCCESS: 200,
-            CREATED: 201,
-            ACCEPTED: 202,
-            BAD_REQUEST: 400,
-            UNAUTHORIZED: 401,
-            FORBIDDEN: 403,
-            NOT_FOUND: 404,
-            THROTTLED: 429,
-            INTERNAL_SERVER_ERROR: 500,
-            BAD_GATEWAY: 502,
-            SERICE_UNAVIALABLE: 503,
-            GATEWAY_TIMEOUT: 504
-        })
+    .constant("HTTP_STATUS", {
+      SUCCESS: 200,
+      CREATED: 201,
+      ACCEPTED: 202,
+      BAD_REQUEST: 400,
+      UNAUTHORIZED: 401,
+      FORBIDDEN: 403,
+      NOT_FOUND: 404,
+      THROTTLED: 429,
+      INTERNAL_SERVER_ERROR: 500,
+      BAD_GATEWAY: 502,
+      SERICE_UNAVIALABLE: 503,
+      GATEWAY_TIMEOUT: 504
+    })
 
-        .constant('COLORS', [
-            // CMD colors first
-            '#FFCC00',
-            '#00AD68',
-            '#EB5D56',
-            '#595959',
+    .constant("COLORS", [
+      // CMD colors first
+      "#FFCC00",
+      "#00AD68",
+      "#EB5D56",
+      "#595959",
 
-            // Other sexy colors next
-            '#00BCD4',
-            '#3F51B5',
-            '#8BC34A',
-            '#f44336',
-            '#FFEB3B',
-            '#03A9F4',
+      // Other sexy colors next
+      "#00BCD4",
+      "#3F51B5",
+      "#8BC34A",
+      "#f44336",
+      "#FFEB3B",
+      "#03A9F4",
 
-            // And some backup colors when things are getting crazy
-            '#FF9800',
-            '#673AB7',
-            '#4CAF50',
-            '#795548',
-            '#9C27B0',
-            '#9E9E9E',
-            '#607D8B',
-            '#FFC107',
-            '#009688',
-            '#2196F3',
-            '#FF5722',
-            '#E91E63',
-        ])
-
-        ; // End of constants
-
-}());
+      // And some backup colors when things are getting crazy
+      "#FF9800",
+      "#673AB7",
+      "#4CAF50",
+      "#795548",
+      "#9C27B0",
+      "#9E9E9E",
+      "#607D8B",
+      "#FFC107",
+      "#009688",
+      "#2196F3",
+      "#FF5722",
+      "#E91E63"
+    ]); // End of constants
+})();
--- src/app/routes/lecturer/guilds/settings/guilds.settings.controller.js
+++ src/app/routes/lecturer/guilds/settings/guilds.settings.controller.js
@@ -1,231 +1,268 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    angular
-        .module('cmd.guilds')
-        .controller('GuildSettingsController', GuildSettingsController);
+  angular
+    .module("cmd.guilds")
+    .controller("GuildSettingsController", GuildSettingsController);
 
-    /** @ngInject */
-    function GuildSettingsController(
-        $mdDialog,
-        $mdToast,
-        $state,
-        $stateParams,
-        Global,
-        Notifications,
-        toastr,
-        Guild,
-        TrelloApi,
-        localStorageService,
-        LECTURER_ACCESS_LEVEL,
-        TRELLO_USER_ID
-    ) {
+  /** @ngInject */
+  function GuildSettingsController(
+    $mdDialog,
+    $mdToast,
+    $state,
+    $stateParams,
+    Global,
+    Notifications,
+    toastr,
+    Guild,
+    TrelloApi,
+    localStorageService,
+    LECTURER_ACCESS_LEVEL
+  ) {
+    if (Global.getAccess() < LECTURER_ACCESS_LEVEL) {
+      return Global.notAllowed();
+    }
 
-        if(Global.getAccess() < LECTURER_ACCESS_LEVEL) {
-            return Global.notAllowed();
+    Global.setRouteTitle("Team instellingen");
+    Global.setRouteBackRoute("base.guilds.overview");
+
+    var self = this;
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			Methods
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    self.deleteGuild = deleteGuild;
+    self.changeGuildName = changeGuildName;
+    self.patchSettings = patchSettings;
+    self.deleteRule = deleteRule;
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			Variables
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    self.guild = [];
+    self.trello_boards = [];
+    self.trello_board_lists = [];
+    self.loading_page = true;
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			Services
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    Guild.getGuild($stateParams.guildUuid).then(
+      function(response) {
+        if (response.status === 404) {
+          toastr.error("Team " + $stateParams.guildUuid + " bestaad niet");
+          $state.go("base.guilds.overview");
         }
-
-        Global.setRouteTitle('Team instellingen');
-        Global.setRouteBackRoute('base.guilds.overview');
-
-        var self = this;
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Methods
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        self.deleteGuild = deleteGuild;
-        self.changeGuildName = changeGuildName;
-        self.patchSettings = patchSettings;
-        self.deleteRule = deleteRule;
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Variables
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        self.guild = [];
-        self.trello_boards = [];
-        self.trello_board_lists = [];
-        self.loading_page = true;
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Services
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        Guild.getGuild($stateParams.guildUuid)
-            .then(function(response) {
-                if(response.status === 404) {
-                    toastr.error('Team ' + $stateParams.guildUuid + ' bestaad niet');
-                    $state.go('base.guilds.overview');
-                }
-                Global.setRouteTitle('Team instellingen ' + response.name);
-                self.guild = response;
-                getGuildRules(self.guild);
-
-                TrelloApi.Authenticate()
-                .then(function(response) {
-                    TrelloApi.Rest('GET', 'members/' + TRELLO_USER_ID + '/boards')
-                    .then(function(response) {
-                        self.trello_boards = response;
-                    });
-                    if(self.guild.trello_board) {
-                        TrelloApi.Rest('GET', 'boards/' + self.guild.trello_board + '/lists')
-                        .then(function(response) {
-                            self.trello_board_lists = response;
-                            self.loading_page = false;
-                        }, function(error) {
-                            // TODO
-                            // Dont delete this on a 404
-                            self.loading_page = false;
-                            self.guild.trello_board = null;
-                            self.guild.trello_done_lists = null;
-                            // Make sure to delete this setting in the backend
-                            // bacause the bord has been deleted
-                            Guild.patchGuildSettings(self.guild)
-                            .then(function(response) {
-                                toastr.info('Trello bord bestaad niet meer, team is geupdate');
-                            })
-                            .catch(function(error) {
-                                //console.log(error);
-                            });
-                        });
-                    } else {
-                      self.loading_page = false;
-                    }
-                }, function(error){
-                    //console.log(error);
-                });
-
-            }, function() {
-                // Err
-            });
-
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Method Declarations
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        function getGuildRules(guild) {
-          var local_guilds = localStorageService.get('guilds') || [];
-
-          if(_.findWhere(local_guilds, {guild: guild.id}) && moment(_.findWhere(local_guilds, {guild: guild.id}).datetime).isAfter(moment().subtract(1, 'minutes'))) {
-            guild.rules = _.findWhere(local_guilds, {guild: guild.id}).rules;
-          } else {
-            Guild.V2getGuildRules(guild.id)
-            .then(function(response) {
-              local_guilds = localStorageService.get('guilds') || [];
-              var local_guild = _.findWhere(local_guilds, {guild: guild.id});
-              var tempObj = {
-                guild: guild.id,
-                datetime: moment(),
-                rules: response.data
-              };
-
-              // Check if we need to update the local storage
-              if(local_guild) {
-                local_guilds[_.indexOf(local_guilds, local_guild)] = tempObj;
-              } else {
-                local_guilds.push(tempObj);
-              }
-
-              localStorageService.set('guilds', local_guilds);
-
-              guild.rules = response.data;
-            })
-            .catch(function(error) {
-              toastr.error(error);
-            });
-          }
-        }
-
-        function deleteGuild(event) {
-            Notifications.confirmation(
-                'Weet je zeker dat je dit team wilt verwijderen?',
-                'Deze actie kan niet meer ongedaan worden.',
-                'Verwijder team',
-                event
-            ).then(function() {
-                Guild.deleteGuild(self.guild.id)
-                .then(function(response) {
-                    toastr.success('Team ' + self.guild.name + ' is verwijdert');
-                    $state.go('base.guilds.overview');
-                }, function() {
-                    // Err deleting guild
-                });
-            }, function() {
-                // Nope Nope Nope Nope
-            });
-        }
-
-        function changeGuildName(event) {
-            Notifications.prompt(
-                'Wijzig teamnaam van "' +self.guild.name+ '"',
-                'Wat word de nieuwe naam van dit team?',
-                'Team naam',
-                event
-            )
-            .then(function(result) {
-                // Checks for thw world name
-                if(!result) {
-                    return toastr.warning('Vul een teamnaam in');
-                }
-
-                Guild.patchGuildName(result, self.guild.id)
-                .then(function(response) {
-                    toastr.success('Team naam gewijzigd naar ' + result);
-                    self.guild.name = result;
-                }, function() {
-                    // Err patch guild name
-                });
-            }, function() {
-                // Cancel
-            });
-        }
-
-        function patchSettings() {
-            if(self.guild.trello_board) {
-                TrelloApi.Rest('GET', 'boards/' + self.guild.trello_board + '/lists')
-                .then(function(response) {
+        Global.setRouteTitle("Team instellingen " + response.name);
+        self.guild = response;
+        getGuildRules(self.guild);
+        if (!response.world.trello_user_id) {
+          toastr.error("Geen trello user ingesteld");
+          $state.go("base.guilds.overview");
+        } else {
+          TrelloApi.Authenticate().then(
+            function(response) {
+              TrelloApi.Rest(
+                "GET",
+                "members/" + self.guild.world.trello_user_id + "/boards"
+              ).then(function(response) {
+                self.trello_boards = response;
+              });
+              if (self.guild.trello_board) {
+                TrelloApi.Rest(
+                  "GET",
+                  "boards/" + self.guild.trello_board + "/lists"
+                ).then(
+                  function(response) {
                     self.trello_board_lists = response;
-                }, function(error){
-                    //console.log(error);
-                });
+                    self.loading_page = false;
+                  },
+                  function(error) {
+                    // TODO
+                    // Dont delete this on a 404
+                    self.loading_page = false;
+                    self.guild.trello_board = null;
+                    self.guild.trello_done_lists = null;
+                    // Make sure to delete this setting in the backend
+                    // bacause the bord has been deleted
+                    Guild.patchGuildSettings(self.guild)
+                      .then(function(response) {
+                        toastr.info(
+                          "Trello bord bestaad niet meer, team is geupdate"
+                        );
+                      })
+                      .catch(function(error) {
+                        //console.log(error);
+                      });
+                  }
+                );
+              } else {
+                self.loading_page = false;
+              }
+            },
+            function(error) {
+              //console.log(error);
+            }
+          );
+        }
+      },
+      function() {
+        // Err
+      }
+    );
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			Method Declarations
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    function getGuildRules(guild) {
+      var local_guilds = localStorageService.get("guilds") || [];
+
+      if (
+        _.findWhere(local_guilds, { guild: guild.id }) &&
+        moment(_.findWhere(local_guilds, { guild: guild.id }).datetime).isAfter(
+          moment().subtract(1, "minutes")
+        )
+      ) {
+        guild.rules = _.findWhere(local_guilds, { guild: guild.id }).rules;
+      } else {
+        Guild.V2getGuildRules(guild.id)
+          .then(function(response) {
+            local_guilds = localStorageService.get("guilds") || [];
+            var local_guild = _.findWhere(local_guilds, { guild: guild.id });
+            var tempObj = {
+              guild: guild.id,
+              datetime: moment(),
+              rules: response.data
+            };
+
+            // Check if we need to update the local storage
+            if (local_guild) {
+              local_guilds[_.indexOf(local_guilds, local_guild)] = tempObj;
+            } else {
+              local_guilds.push(tempObj);
             }
 
-            Guild.patchGuildSettings(self.guild)
-            .then(function(response) {
-                toastr.success('Team instellingen opgeslagen');
-            })
-            .catch(function(error) {
-                //console.log(error);
-            });
-        }
+            localStorageService.set("guilds", local_guilds);
 
-        function deleteRule(rule) {
-          if(Global.getLocalSettings().enabled_confirmation) {
-            Notifications.confirmation(
-                'Weet je zeker dat je deze regel wilt verwijderen?',
-                'Deze actie kan niet meer ongedaan worden.',
-                'verwijder regel',
-                event
-            )
-            .then(function() {
-                removeGuildRule(rule);
-            }, function() {
-                // No
-            });
-          } else {
-            removeGuildRule(rule);
-          }
-        }
-
-        function removeGuildRule(rule) {
-          Guild.removeGuildRule(rule)
-          .then(function(response) {
-            self.guild.rules.splice(self.guild.rules.indexOf(_.findWhere(self.guild.rules, { id: rule.id})), 1);
-            toastr.success("Regel is verwijdert");
+            guild.rules = response.data;
           })
           .catch(function(error) {
             toastr.error(error);
           });
-        }
-
-
+      }
     }
-}());
+
+    function deleteGuild(event) {
+      Notifications.confirmation(
+        "Weet je zeker dat je dit team wilt verwijderen?",
+        "Deze actie kan niet meer ongedaan worden.",
+        "Verwijder team",
+        event
+      ).then(
+        function() {
+          Guild.deleteGuild(self.guild.id).then(
+            function(response) {
+              toastr.success("Team " + self.guild.name + " is verwijdert");
+              $state.go("base.guilds.overview");
+            },
+            function() {
+              // Err deleting guild
+            }
+          );
+        },
+        function() {
+          // Nope Nope Nope Nope
+        }
+      );
+    }
+
+    function changeGuildName(event) {
+      Notifications.prompt(
+        'Wijzig teamnaam van "' + self.guild.name + '"',
+        "Wat word de nieuwe naam van dit team?",
+        "Team naam",
+        event
+      ).then(
+        function(result) {
+          // Checks for thw world name
+          if (!result) {
+            return toastr.warning("Vul een teamnaam in");
+          }
+
+          Guild.patchGuildName(result, self.guild.id).then(
+            function(response) {
+              toastr.success("Team naam gewijzigd naar " + result);
+              self.guild.name = result;
+            },
+            function() {
+              // Err patch guild name
+            }
+          );
+        },
+        function() {
+          // Cancel
+        }
+      );
+    }
+
+    function patchSettings() {
+      if (self.guild.trello_board) {
+        TrelloApi.Rest(
+          "GET",
+          "boards/" + self.guild.trello_board + "/lists"
+        ).then(
+          function(response) {
+            self.trello_board_lists = response;
+          },
+          function(error) {
+            //console.log(error);
+          }
+        );
+      }
+
+      Guild.patchGuildSettings(self.guild)
+        .then(function(response) {
+          toastr.success("Team instellingen opgeslagen");
+        })
+        .catch(function(error) {
+          //console.log(error);
+        });
+    }
+
+    function deleteRule(rule) {
+      if (Global.getLocalSettings().enabled_confirmation) {
+        Notifications.confirmation(
+          "Weet je zeker dat je deze regel wilt verwijderen?",
+          "Deze actie kan niet meer ongedaan worden.",
+          "verwijder regel",
+          event
+        ).then(
+          function() {
+            removeGuildRule(rule);
+          },
+          function() {
+            // No
+          }
+        );
+      } else {
+        removeGuildRule(rule);
+      }
+    }
+
+    function removeGuildRule(rule) {
+      Guild.removeGuildRule(rule)
+        .then(function(response) {
+          self.guild.rules.splice(
+            self.guild.rules.indexOf(
+              _.findWhere(self.guild.rules, { id: rule.id })
+            ),
+            1
+          );
+          toastr.success("Regel is verwijdert");
+        })
+        .catch(function(error) {
+          toastr.error(error);
+        });
+    }
+  }
+})();
